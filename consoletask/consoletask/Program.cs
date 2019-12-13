a066b0aad8549a2af512a765fcb17a37b3abedd3
saskovets nikolay <saskovetsnikolay@gmail.com>
2019-12-13 18:27:44 +0300

    HW1 commit

---
+++ consoletask/consoletask/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoletask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.Title = "HW1 task";
+
+            Console.WriteLine("Changed Title: {0}", Console.Title);
+
+            Console.WriteLine("Nikolay Sakovets");
+
+            int age = 28;
+
+            Console.WriteLine($"I live in Minsk and I\'m {age}");
+
+            Console.ReadKey();
+        }
+    }
+}

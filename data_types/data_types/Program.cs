e7f50cad2522f7812d279fdb0003de04aa5c73c8
saskovets nikolay <saskovetsnikolay@gmail.com>
2019-12-15 05:03:06 +0300

    redifinition

---
+++ data_types/data_types/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int b = Convert.ToInt32(a);
+            float c = (float)b;
+            long d = (long)b;
+            decimal e = (decimal)b;
+            double f = (double)b;
+
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+        }
+    }
+}

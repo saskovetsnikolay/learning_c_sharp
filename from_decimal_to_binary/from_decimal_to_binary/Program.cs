3ad8397afc01caaeb66a95549d51b1123b40362a
saskovets nikolay <saskovetsnikolay@gmail.com>
2019-12-18 22:04:26 +0300

    part1

---
+++ from_decimal_to_binary/from_decimal_to_binary/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+            * Exceptions
+            * Arrays and Lists
+            * Decimal to Binary
+            * Formatting string
+            * Binary to Decimal
+            */
+
+/*
+ * Создать программу которая будет считывать с консоли число
+ * Если число меньше нуля или дробное кидать ошибку с текстом "Not Right Number Format"
+ * Иначе будет создавать массив с двоичным представлением числа, выводить число (в двоичном виде)
+ * в консоль видом: Binary number: 010101
+ * А после будет удалять из массива все нули и выводить в консоль видом: Binary number: 111
+ * А потом выводить "Decimal from only ones : 7" *********
+ */
+
+namespace from_decimal_to_binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers;
+            int number = Convert.ToInt32(Console.ReadLine());
+            float c = (float)number;
+            //string b = Convert.ToString(number, 2);
+            int a = 0;
+            int i = 0;
+            numbers = new int[32];
+            while (number >= 1)
+            {
+                a = number % 2;
+                numbers[i] = a;
+                i++;
+                number = number / 2;
+                Console.WriteLine(a);
+            }
+                //Console.WriteLine(b);
+                if (a == 0 & a == c)
+            {
+                Console.WriteLine("Number format isn\'t right.");
+            }
+           
+
+        }
+    }
+}
+/*int item = Convert.ToInt32(b);
+numbers = new int[item];
+            Console.WriteLine(numbers);*/
---
+++ from_decimal_to_binary/from_decimal_to_binary/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("from_decimal_to_binary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("from_decimal_to_binary")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f6a77374-f92f-4b33-9f0f-12cb196791ac")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
